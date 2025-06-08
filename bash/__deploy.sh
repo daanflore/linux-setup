1bd03961fd0e5e877bed7ff684d151aeecc702d1
daanflore <daanflore@gmail.com>
2025-06-08 23:33:19 +0200

    work with soft links

--- bash/__deploy.sh
+++ bash/__deploy.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
 rm ~/.bash_profile
-ln bash_profile ~/.bash_profile
+ln -s $SCRIPT_DIR/bash_profile ~/.bash_profile
 
 rm ~/.bashrc
-ln bashrc ~/.bashrc
+ln -s $SCRIPT_DIR/bashrc ~/.bashrc
 
 rm ~/.dircolors
-ln dircolors ~/.dircolors
+ln -s $SCRIPT_DIR/dircolors ~/.dircolors
