ce9e56945fac58a3fe85183ba6517674ee89ef26
daanflore <daanflore@gmail.com>
2025-06-22 14:03:40 +0200

    Adding molecule

--- install-ansible.sh
+++ install-ansible.sh
@@ -1,6 +1,7 @@
 sudo apt update
-sudo apt install software-properties-common
+sudo apt install -y software-properties-common
 sudo add-apt-repository --yes --update ppa:ansible/ansible
-sudo apt install ansible
-sudo apt install ansible-lint
+sudo apt install -y ansible
+sudo apt install -y ansible-lint
 sudo apt install -y python3-pip libssl-dev
+pip3 install ansible-dev-tools
